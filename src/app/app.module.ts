3b406bfac7b042e9c8ae42fe4133ec055a293fce
Thomas Molenaar <tlmolenaar@hotmail.com>
2017-02-22 22:37:44 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyASySrC3ivy4CVTG1GJwbyQwc84L0bH1W4",
+    authDomain: "businesscontacts-abce0.firebaseapp.com",
+    databaseURL: "https://businesscontacts-abce0.firebaseio.com",
+    storageBucket: "businesscontacts-abce0.appspot.com",
+    messagingSenderId: "904356054402"
+  };
+
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
